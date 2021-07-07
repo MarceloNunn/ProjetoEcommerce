a4b3222f1f04b5be2996e104dc43fdeaa51b2dbe
MarceloNunn <marfernes025@gmail.com>
2021-07-07 13:40:18 -0400

    Primeiro commit

--- ApplicationApp/OpenApp/AppProduct.cs
+++ ApplicationApp/OpenApp/AppProduct.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using ApplicationApp.Interfaces;
+using Entities.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 
 namespace ApplicationApp.OpenApp
 {
-    public class AppProduct
+    public class AppProduct : InterfaceProductApp
     {
+        
+         
+        public AppProduct()
+        {
+                
+        }
+        public Task Add(Product Objeto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task DeleteProduct(Product Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetByIdProduct(Product Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> List()
+        {
+            throw new NotImplementedException();
+        }
+        public Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
